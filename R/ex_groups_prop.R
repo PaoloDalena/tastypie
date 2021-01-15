1a53e895bc4a233a8b07355ce37c004360795fcb
Paolo Dalena <paolodalena97@gmail.com>
2021-01-15 17:36:22 +0100

    remove ex_groups_prop

--- R/ex_groups_prop.R
+++
@@ -1,7 +0,0 @@
-#' @importFrom magrittr %>%
-ex_groups_prop <- function(n){
-  ex <- ex_groups(n) %>%
-    arrange(desc(group)) %>%
-    mutate(prop = value / sum(ex$value)*100) %>%
-    mutate(ypos = cumsum(prop)- 0.5*prop )
-}
