b088f0550f407a1f34e44808e713e5b0900696f2
Paolo Dalena <paolodalena97@gmail.com>
2021-01-20 15:54:45 +0100

    add proper data doc

--- R/data.R
+++ R/data.R
@@ -1,5 +1,7 @@
-#' Available templates in \code{tastypie}.
+#' Available templates in \code{tastypie} for \code{pie_bake}.
 #'
-#' The vector containing all the available templates for pie charts for the \code{tastypie}
-#' package.
+#' The vector containing all the available templates for pie charts for the
+#' \code{\link{pie_bake}} function.
+#' Other more complex templates are available in \code{\link{pie_template_list_pro}}
+#' that can be used through the \code{\link{pie_bake_pro}} function.
 "pie_template_list"
---
+++ R/data_2.R
@@ -0,0 +1,7 @@
+#' Available templates in \code{tastypie} for \code{pie_bake_pro}.
+#'
+#' The vector containing all the available templates for pie charts for the
+#' \code{\link{pie_bake_pro}} function.
+#' Other less complex templates are available in \code{\link{pie_template_list}}
+#' that can be used through the \code{\link{pie_bake}} function.
+"pie_template_list_pro"
