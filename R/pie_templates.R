3716ab90371ff628dc963c2994ebcd1ce61f28f7
Paolo Dalena <paolodalena97@gmail.com>
2021-01-20 16:40:01 +0100

    improve pie_templates fun

--- R/pie_templates.R
+++ R/pie_templates.R
@@ -1,12 +1,26 @@
 pie_templates <- function(template, n_groups = 4, perc = FALSE){
-  if(!template %in% pie_template_list){
-    stop("\n The selected template does NOT exist. \n Type pie_template_list to see all the available templates.")
+  if(!template %in% c(pie_template_list, pie_template_list_pro)){
+    stop("\n The selected template does NOT exist.
+ Type pie_template_list and pie_template_list_pro to see all the available templates.")
   }
-  if(!perc){
-    pie_bake(ex_groups(n_groups), template = template, title = paste("template = ",template))
+  else if(template %in% pie_template_list){
+    if(!perc){
+      pie_bake(ex_groups(n_groups), template = template,
+               title = paste(
+                 "pie_bake( ... , template = '",template, "')", sep = "")
+               )
+    }
+    else if(perc){
+      pie_bake(ex_groups(n_groups), template = template, perc = TRUE,
+               title = paste(
+                 "pie_bake( ... , template = '",template, "', perc = TRUE)", sep = "")
+               )
+    }
   }
-  else if(perc){
-    pie_bake(ex_groups(n_groups), template = template, perc = TRUE,
-             title = paste("template = ",template, ", perc = TRUE", sep = ""))
+  else if(template %in% pie_template_list_pro){
+    pie_bake_pro(ex_groups(n_groups), template = template,
+             title = paste(
+               "pie_bake_pro( ... , template = '",template, "')", sep = "")
+             )
   }
 }
