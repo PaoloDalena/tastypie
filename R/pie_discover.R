2e45c53c3e7d68fc48cc2462b40186316cf11dc6
Paolo Dalena <paolodalena97@gmail.com>
2021-01-20 16:43:53 +0100

    improve pie_discover fun

--- R/pie_discover.R
+++ R/pie_discover.R
@@ -1,5 +1,21 @@
-pie_discover <- function(){
-  templ <- sample(pie_template_list, 1)
+pie_discover <- function(
+  which = c("all", "bake", "pro")
+){
+
+  if(missing(which)){
+    which <- "all"
+  }
+
+  if(which == "all"){
+    templ <- sample(c(pie_template_list, pie_template_list_pro), 1)
+  }
+  else if(which == "bake"){
+    templ <- sample(pie_template_list, 1)
+  }
+  else if(which == "pro"){
+    templ <- sample(pie_template_list_pro, 1)
+  }
+
   n <- sample(2:5, 1)
   pe <- sample(0:1, 1)
   pie_templates(template = templ, n_groups = n, perc = pe)
