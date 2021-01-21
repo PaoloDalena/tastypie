725998c4cc158dc8a822ec99fe42a0e672dee14e
Paolo Dalena <paolodalena97@gmail.com>
2021-01-21 19:32:39 +0100

    improve ex_groups to max 9 groups

--- R/ex_groups.R
+++ R/ex_groups.R
@@ -9,9 +9,21 @@ ex_groups <- function(n){
     ex <- data.frame("group" = LETTERS[1:n], "value" = c(40, 25, 20, 15))
   }
   else if(n == 5){
-    ex <- data.frame("group" = LETTERS[1:n], "value" = c(30, 25, 20, 15, 10))
+    ex <- data.frame("group" = LETTERS[1:n], "value" = c(30, 25, 18, 15, 10))
+  }
+  else if(n == 6){
+    ex <- data.frame("group" = LETTERS[1:n], "value" = c(22, 25, 11, 15, 10, 8))
+  }
+  else if(n == 7){
+    ex <- data.frame("group" = LETTERS[1:n], "value" = c(43, 22, 30, 25, 20, 15, 10))
+  }
+  else if(n == 8){
+    ex <- data.frame("group" = LETTERS[1:n], "value" = c(30, 25, 20, 15, 10, 12, 32, 19))
+  }
+  else if(n == 9){
+    ex <- data.frame("group" = LETTERS[1:n], "value" = c(30, 25, 20, 15, 10, 32, 42, 18, 49))
   }
   else{
-    stop("The argument should be a number from 2 to 5.")
+    stop("The argument should be a number from 2 to 9.")
   }
 }
