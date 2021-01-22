dd820f515d2177aeaf4be8bb125a2e20b1bf17af
Paolo Dalena <paolodalena97@gmail.com>
2021-01-22 03:07:01 +0100

    improve pie_discover up to 9 groups

--- R/pie_discover.R
+++ R/pie_discover.R
@@ -33,7 +33,7 @@ pie_discover <- function(
     templ <- sample(pie_template_list_pro, 1)
   }
 
-  n <- sample(2:5, 1)
+  n <- sample(2:9, 1)
   pe <- sample(0:1, 1)
   pie_templates(template = templ, n_groups = n, perc = pe)
 }
