cb5ed313821b097417656ba41cb6182062a83ad9
Paolo Dalena <paolodalena97@gmail.com>
2021-01-22 01:18:54 +0100

    add package documentation

---
+++ R/tastypie.R
@@ -0,0 +1,23 @@
+#' Easy pie charts with \code{tastypie}
+#'
+#'
+#' @description
+#' ![](logo.png "options: align = 'right' alt = 'logo' width = 120")
+#' You only need to type
+#' \href{https://www.google.com/search?q=why+pie+charts+are+bad}{why pie charts are bad}
+#' on Google to find thousands of articles full of (*valid*) reasons why other types of charts
+#' should be preferred over this one.\cr
+#' Therefore, because of the little use due to the reasons already mentioned,
+#' making pie charts (and related) in R is not straightforward, so other functions are needed
+#' to simplify things.\cr
+#' In this R package there are useful functions to make *tasty* pie charts immediately
+#' by exploiting the many cool templates provided.\cr\cr
+#' Find more information on the GitHub package page
+#' \href{https://github.com/PaoloDalena/tastypie}{here}.
+#'
+#' @author
+#' \strong{Author and Maintainter}: Paolo Dalena \email{paolodalena97@gmail.com}
+#'
+#' @docType package
+#' @name tastypie
+NULL
